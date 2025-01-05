dbed8d981c08a6081b6bd71467dfb733e1060fbe
Bryan Kaylor <brskaylor@gmail.com>
2025-01-05 12:39:00 -0600

    Update build script to not pushd popd

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-::@echo off
+@echo off
 
 set "OPTIMIZE_FLAG=/Od"
 if "%1"=="optimized" (
@@ -10,7 +10,8 @@ if "%1"=="optimized" (
   set "CUDA_OPTIMIZE_FLAG=-O3 --use_fast_math -arch=sm_86"
 )
 
-pushd bin
-nvcc -Xcompiler "/W4 /WX" -c %CUDA_OPTIMIZE_FLAG% -allow-unsupported-compiler -ccbin "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.40.33807\bin\Hostx64\x64" ..\src\voronoi.cu
-cl ..\src\main.c voronoi.obj /Fevoronoi.exe /Zi %OPTIMIZE_FLAG% /W4 /WX /I..\msvc_sdl\SDL2-2.0.9\include /I..\msvc_sdl\SDL2_ttf-2.0.15\include /I..\msvc_sdl\SDL2_image-2.0.4\include /I "%cuda_path%\include" /link /PROFILE /LIBPATH:..\msvc_sdl\SDL2-2.0.9\lib\x64 /LIBPATH:..\msvc_sdl\SDL2_ttf-2.0.15\lib\x64 /LIBPATH:..\msvc_sdl\SDL2_image-2.0.4\lib\x64 /LIBPATH:"%cuda_path%\lib\x64" /SUBSYSTEM:CONSOLE "SDL2_ttf.lib" "SDL2_image.lib" "SDL2main.lib" "SDL2.lib" "cudart.lib"
-popd
+if not exist bin mkdir bin
+
+nvcc -Xcompiler "/W4 /WX" -c %CUDA_OPTIMIZE_FLAG% -allow-unsupported-compiler -ccbin "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.40.33807\bin\Hostx64\x64" src\voronoi.cu -o bin\voronoi.obj
+
+cl src\main.c bin\voronoi.obj /Febin\voronoi.exe /Fobin\ /Zi %OPTIMIZE_FLAG% /W4 /WX /I.\msvc_sdl\SDL2-2.0.9\include /I.\msvc_sdl\SDL2_ttf-2.0.15\include /I.\msvc_sdl\SDL2_image-2.0.4\include /I "%cuda_path%\include" /link /PROFILE /LIBPATH:.\msvc_sdl\SDL2-2.0.9\lib\x64 /LIBPATH:.\msvc_sdl\SDL2_ttf-2.0.15\lib\x64 /LIBPATH:.\msvc_sdl\SDL2_image-2.0.4\lib\x64 /LIBPATH:"%cuda_path%\lib\x64" /SUBSYSTEM:CONSOLE "SDL2_ttf.lib" "SDL2_image.lib" "SDL2main.lib" "SDL2.lib" "cudart.lib"
